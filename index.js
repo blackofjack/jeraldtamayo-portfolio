d9c1449b5e4e30f6331696cac0c405913eca47d7
blackofjack <jeraldtamayo.ph@gmail.comjeraldtamayo.ph@gmail.comgit config --global user.email jeraldtamayo.ph@gmail.com>
2026-04-05 20:52:12 +0800

    feat:web dev portfolio

---
+++ index.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".toggle-hamburger");
+    const navLinks = document.querySelector(".menu");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        hamburger.classList.toggle("active"); 
+    });
+});
+
+//api call
+
+(function() {
+  emailjs.init("A7OqR4LJyL1_59JIb"); // 👈 replace this
+})();
+
+emailjs.init("A7OqR4LJyL1_59JIb");
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_y5ele2a",
+    "template_b5tcwlu",
+    this
+  )
+  .then(() => {
+    alert("Message sent 🚀");
+    form.reset();
+  })
+  .catch((err) => {
+    console.log(err);
+    alert("Failed 😢");
+  });
+});
+
+//floating button
+
+window.onscroll = function() {
+  const btn = document.getElementById("floatingBtn");
+  const middle = document.documentElement.scrollHeight / 4;
+
+  // Show button when user scrolls past the middle of the page
+  if (window.scrollY > middle) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
